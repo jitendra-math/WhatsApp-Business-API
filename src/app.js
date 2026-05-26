70c35d549f77442f2980d95614142855f7795834
Jitendra Singh <i.jitendra.singh0@gmail.com>
2026-05-26 13:33:05 +0530

    Update via Repo Manager 🚀

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+// src/app.js
 import express from 'express';
+import cors from 'cors';
 import apiRoutes from './routes/api.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
